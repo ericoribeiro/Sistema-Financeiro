8e4cd0c1e2a72c04d0cd352c2b7a1413e95698a3
Erico Ribeiro <ericor25@gmail.com>
2017-10-27 10:11:24 -0200

    Commit complementar da Base do Projeto

---
+++ PrjSistemaFinanceiro/Model.Dao/ConexaoDB.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+namespace Model.Entity
+{
+    class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDB = null;
+        private SqlConnection con;
+
+        private ConexaoDB()
+        {
+            con = new SqlConnection("Data Source=DESKTOP-PEDV6C1; Initial Catalog=financeiro; Integrated Security=True");
+        }
+
+        public static ConexaoDB saberEstado()
+        {
+            if (objConexaoDB == null)
+            {
+                objConexaoDB = new ConexaoDB();
+            }
+
+            return objConexaoDB;
+        }
+
+        public SqlConnection getCon()
+        {
+            return con;
+        }
+
+        public void CloseDB()
+        {
+            objConexaoDB = null;
+        }
+    }
+}
