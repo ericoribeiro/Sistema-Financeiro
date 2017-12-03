0e8eaebd378a1dc369e8b97483110fe18952e736
Erico Ribeiro <ericor25@gmail.com>
2017-12-03 16:06:14 -0200

    Inicio do Crud de Vendedor

---
+++ PrjSistemaFinanceiro/Model.Dao/VendedorDao.cs
@@ -0,0 +1,78 @@
+﻿using Model.Entity;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System;
+
+namespace Model.Dao
+{
+    public class VendedorDao : Obrigatorio<Vendedor>
+    {
+        private ConexaoDB objConexaoDB;
+        private SqlCommand comando;
+
+        public VendedorDao()
+        {
+            objConexaoDB = ConexaoDB.saberEstado();
+        }
+
+        public void create(Vendedor obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void delete(Vendedor obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool find(Vendedor obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Vendedor> findAll()
+        {
+            List<Vendedor> listVendedores = new List<Vendedor>();
+            string query = "@SELECT * FROM vendedor ORDER BY ASC";
+
+            try
+            {
+                comando = new SqlCommand(query, objConexaoDB.getCon());
+                objConexaoDB.getCon().Open();
+                SqlDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Vendedor vendedor = new Vendedor();
+                    vendedor.IdVendedor = Convert.ToInt64(reader[0].ToString());
+                    vendedor.Nome = reader[1].ToString();
+                    vendedor.CPF = reader[2].ToString();
+                    vendedor.Telefone = reader.ToString();
+                    listVendedores.Add(vendedor);
+                }
+            }
+            catch (Exception ex)
+            {
+                var mensagem = String.Format("Não foi possível listar os Vendedores");
+                throw new Exception(mensagem, ex);
+            }
+            finally
+            {
+                objConexaoDB.getCon().Close();
+                objConexaoDB.CloseDB();
+            }
+
+            return listVendedores;
+        }
+
+        public void procedureCreate(Vendedor obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void update(Vendedor obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PrjSistemaFinanceiro/Model.Entity/Vendedor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entity
+{
+    public class Vendedor
+    {
+        private long idVendedor;
+        private string nome;
+        private string cpf;
+        private string telefone;
+
+        //Encapsulamento dos atributos privados
+
+        public long IdVendedor { get => idVendedor; set => idVendedor = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string CPF { get => cpf; set => cpf = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+
+        //Métodos da classe
+
+        public Vendedor() {
+
+        }
+        public Vendedor(long IdVendedor)
+        {
+            this.idVendedor = IdVendedor;
+        }
+    }
+}
